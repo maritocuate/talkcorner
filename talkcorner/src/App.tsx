aadaceaa8bb46b84caa8cbdec0b27e4e865a0dc9
maritocuate <mario300@gmail.com>
2024-04-22 17:18:20 -0300

    ✨ emit message

--- talkcorner/src/App.tsx
+++ talkcorner/src/App.tsx
@@ -1,12 +1,53 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import io from 'socket.io-client'
+import { Input } from './components/ui/input'
+import { Button } from './components/ui/button'
 
 const socket = io('/')
 
 function App() {
+  const [messages, setMessages] = useState([])
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    socket.on('message', receiveMessage)
+
+    return () => {
+      socket.off('message', receiveMessage)
+    }
+  }, [])
+
+  const receiveMessage = (message: string) => {
+    setMessages(prev => [message, ...prev])
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const newMessage = {
+      body: message,
+      from: 'Me',
+    }
+
+    setMessages(prev => [newMessage, ...prev])
+    setMessage('')
+    socket.emit('message', newMessage.body)
+  }
+
   return (
     <>
-      <div>HW</div>
+      <form onSubmit={handleSubmit}>
+        <Input
+          placeholder="Enter message"
+          onChange={e => setMessage(e.target.value)}
+        />
+        <Button>Send</Button>
+      </form>
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>{message.body}</li>
+        ))}
+      </ul>
     </>
   )
 }
