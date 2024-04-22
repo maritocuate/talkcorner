e5ebe39747e100f0166c9fc09d6a06128cf0878a
maritocuate <mario300@gmail.com>
2024-04-22 17:45:19 -0300

    ✨ ts build fix

--- talkcorner/src/App.tsx
+++ talkcorner/src/App.tsx
@@ -3,12 +3,13 @@ import './App.css'
 import io from 'socket.io-client'
 import { Input } from './components/ui/input'
 import { Button } from './components/ui/button'
+import { Message } from './interfaces'
 
 const socket = io('/')
 
 function App() {
-  const [messages, setMessages] = useState([])
-  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState<Message[]>([])
+  const [message, setMessage] = useState<string>('')
 
   useEffect(() => {
     socket.on('message', receiveMessage)
@@ -18,13 +19,13 @@ function App() {
     }
   }, [])
 
-  const receiveMessage = (message: string) => {
+  const receiveMessage = (message: Message) => {
     setMessages(prev => [message, ...prev])
   }
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    const newMessage = {
+    const newMessage: Message = {
       body: message,
       from: 'Me',
     }
@@ -32,6 +33,7 @@ function App() {
     setMessages(prev => [newMessage, ...prev])
     setMessage('')
     socket.emit('message', newMessage.body)
+    console.log(messages)
   }
 
   return (
@@ -40,11 +42,13 @@ function App() {
         <Input
           placeholder="Enter message"
           onChange={e => setMessage(e.target.value)}
+          value={message}
+          autoFocus
         />
         <Button>Send</Button>
       </form>
       <ul>
-        {messages.map((message, index) => (
+        {messages.map((message: Message, index) => (
           <li key={index}>
             <b>{message.from}</b>:{message.body}
           </li>
