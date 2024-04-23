a5ee535cadd9a99e0b73a8dbf55bc1cc995bb989
maritocuate <mario300@gmail.com>
2024-04-23 19:55:42 -0300

    ✨ mobile version

--- talkcorner/src/App.tsx
+++ talkcorner/src/App.tsx
@@ -15,17 +15,37 @@ const socket: Socket = io('https://glib-chief-august.glitch.me')
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <ResizablePanelGroup direction="horizontal" className="h-screen">
-        <ResizablePanel>
-          <UsersList socket={socket} />
-        </ResizablePanel>
+      {window.innerWidth < 768 ? (
+        <ResizablePanelGroup
+          className="md:hidden h-screen"
+          direction="vertical"
+        >
+          <ResizablePanel defaultSize={25}>
+            <UsersList socket={socket} />
+          </ResizablePanel>
 
-        <ResizableHandle withHandle />
+          <ResizableHandle withHandle />
 
-        <ResizablePanel className="p-6">
-          <UserPanel socket={socket} />
-        </ResizablePanel>
-      </ResizablePanelGroup>
+          <ResizablePanel className="p-6">
+            <UserPanel socket={socket} />
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      ) : (
+        <ResizablePanelGroup
+          className="hidden md:block h-screen"
+          direction="horizontal"
+        >
+          <ResizablePanel>
+            <UsersList socket={socket} />
+          </ResizablePanel>
+
+          <ResizableHandle withHandle />
+
+          <ResizablePanel className="p-6">
+            <UserPanel socket={socket} />
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      )}
     </ThemeProvider>
   )
 }
