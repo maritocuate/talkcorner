8abe0537fa64859ce98dcb9be5120c37c4d12c2a
maritocuate <mario300@gmail.com>
2024-04-23 19:28:34 -0300

    ✨ new user toast

--- talkcorner/src/App.tsx
+++ talkcorner/src/App.tsx
@@ -9,7 +9,7 @@ import io, { Socket } from 'socket.io-client'
 import UserPanel from './components/UserPanel'
 import UsersList from './components/UsersList'
 
-const socket = io('https://glib-chief-august.glitch.me')
+const socket: Socket = io('https://glib-chief-august.glitch.me')
 //const socket: Socket = io('http://localhost:3000')
 
 function App() {
