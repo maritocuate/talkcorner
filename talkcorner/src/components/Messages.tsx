fc2cf92ee1bfd2abc9ca85cbeabbb783924452f0
maritocuate <mario300@gmail.com>
2024-04-23 21:07:12 -0300

    ✨ messages update

--- talkcorner/src/components/Messages.tsx
+++ talkcorner/src/components/Messages.tsx
@@ -4,8 +4,11 @@ export default function Messages({ messages }: { messages: Message[] }) {
   return (
     <ul className="space-y-1 text-left p-3">
       {messages.map((message, index) => (
-        <li key={index} className="py-2">
-          <b className="text-primary">{message.from}</b>: {message.body}
+        <li key={index} className="py-2 flex flex-col">
+          <span className="text-sm text-muted-foreground">
+            {message.from} says:
+          </span>
+          <span>{message.body}</span>
         </li>
       ))}
     </ul>
