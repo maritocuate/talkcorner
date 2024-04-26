bd5a1045225f9eefb1ce9e25dc87bea3dd9c23fd
maritocuate <mario300@gmail.com>
2024-04-26 17:17:18 -0300

    ✨ limit length messages

--- talkcorner/src/components/Messages.tsx
+++ talkcorner/src/components/Messages.tsx
@@ -1,11 +1,13 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Message } from '@/interfaces'
 
 export default function Messages({ messages }: { messages: Message[] }) {
+  const [localMessages, setLocalMessages] = useState<Message[]>([])
   const messagesEndRef = useRef<HTMLUListElement>(null)
 
   useEffect(() => {
     scrollToBottom()
+    setLocalMessages(messages.slice(-40))
   }, [messages])
 
   const scrollToBottom = () => {
@@ -19,7 +21,7 @@ export default function Messages({ messages }: { messages: Message[] }) {
       ref={messagesEndRef}
       className="space-y-1 text-left p-3 overflow-y-scroll"
     >
-      {messages.map((message, index) => (
+      {localMessages.map((message, index) => (
         <li key={index} className="py-2 flex flex-col">
           <span className="text-sm text-muted-foreground">
             {message.from} says:
