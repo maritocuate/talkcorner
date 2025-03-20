34a0b5a079a865852d50264c5453118d13715cbf
maritocuate <mario300@gmail.com>
2025-03-19 19:37:50 -0500

    🐞 scrollToBottom method fix

--- talkcorner/src/components/Messages.tsx
+++ talkcorner/src/components/Messages.tsx
@@ -1,11 +1,11 @@
-import { useLayoutEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Message } from '@/interfaces'
 
 export default function Messages({ messages }: { messages: Message[] }) {
   const [localMessages, setLocalMessages] = useState<Message[]>([])
   const messagesEndRef = useRef<HTMLUListElement>(null)
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     scrollToBottom()
     setLocalMessages(messages.slice(-40))
   }, [messages])
