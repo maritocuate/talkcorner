b4ff02dee832f2abf83c8fbad504478c190b3d12
maritocuate <mario300@gmail.com>
2025-03-19 20:06:45 -0500

    ✨ user name styles update

--- talkcorner/src/components/Messages.tsx
+++ talkcorner/src/components/Messages.tsx
@@ -3,12 +3,19 @@ import { Message } from '@/interfaces'
 
 export default function Messages({ messages }: { messages: Message[] }) {
   const [localMessages, setLocalMessages] = useState<Message[]>([])
+  const [userName, setUserName] = useState<string>('')
   const messagesEndRef = useRef<HTMLUListElement>(null)
 
   useLayoutEffect(() => {
+    checkUser()
     setLocalMessages(messages.slice(-40))
     setTimeout(scrollToBottom, 0)
   }, [messages])
+
+  const checkUser = () => {
+    const localUserName = localStorage.getItem('local-username')
+    if (localUserName) setUserName(localUserName)
+  }
 
   const scrollToBottom = () => {
     if (messagesEndRef.current) {
@@ -22,9 +29,14 @@ export default function Messages({ messages }: { messages: Message[] }) {
       className="space-y-1 text-left p-3 overflow-y-scroll"
     >
       {localMessages.map((message, index) => (
-        <li key={index} className="py-2 flex flex-col">
+        <li
+          key={index}
+          className={`py-2 flex flex-col ${
+            message.from === userName ? 'items-end' : ''
+          }`}
+        >
           <span className="text-sm text-muted-foreground">
-            {message.from} says:
+            {message.from === userName ? 'You say:' : message.from + ' says:'}
           </span>
           <span>{message.body}</span>
         </li>
